855e900912ccee3fab3d5198037e6fdbf2245209
luismetzger <luismetzger@me.com>
2016-08-29 14:06:02 -0600

    Finished homework.

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -16,16 +16,7 @@ angular.module('appTwo', [])
         this.enrolled_students = 16;
         this.start_date = "June 12, 2016";
         this.end_date = "Septempber 4, 2016";
-    }
-
-    function daysRemaining() {
-        var oneDay = 24 * 60 * 60 * 1000;
-        var firstDate = new Date(2016, 06, 12);
-        var secondDate = new Date(2016, 09, 04);
-
-        var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay) ));
-
-        console.log(diffDays);
+        this.days_left = "15";
     }
 
 // console.log('challenge #2 -- app.js loaded!');
